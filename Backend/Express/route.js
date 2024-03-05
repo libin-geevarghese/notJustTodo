9e2ed7d992c10d8c2e9954a4730ec3245915fe79
Libin Geevarghese <lbngv7@gmail.com>
2024-03-05 17:42:56 +0530

    Update route.js

--- Backend/Express/route.js
+++ Backend/Express/route.js
@@ -1,36 +1,4 @@
-// const express = require("express");
-// const User = require("../Mongoose/userSchema");
-// const Todo = require("../Mongoose/todoSchema");
-// const router = express.Router();
 
-// //  create a new user
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const newUser = new User(req.body); // Assuming you send user details in the request body
-//     await newUser.save();
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// //  to get all todos
-// router.get("/todos", async (req, res) => {
-//   try {
-//     const todos = await Todo.find();
-//     res.status(200).json(todos);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// // ... Other routes ...
-
-// module.exports = router;
-
-// route.js
 
 const express = require("express");
 const User = require("../Mongoose/userSchema");
@@ -72,6 +40,6 @@ router.get("/todos", async (req, res) => {
   }
 });
 
-// ... Other routes ...
+
 
 module.exports = router;
